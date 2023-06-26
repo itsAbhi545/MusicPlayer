e75f3c0151502fca121f06e80c7fd74c96dbadbc
Harman <harmanjyot.singh@chicmic.co.in>
2023-06-26 11:13:04 +0530

    CR: new users class for signup/login

---
+++ src/main/java/com/example/MusicPlayer/model/Users.java
@@ -0,0 +1,25 @@
+package com.example.MusicPlayer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter@Setter
+@RequiredArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private boolean verified;
+    private String uuid;
+}
